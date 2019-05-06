49f335794a32f7509f398bf65829ae22812cf16d
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-05-06 11:25:54 -0400

    changed post model to have tags as arrays

--- src/models/post_model.js
+++ src/models/post_model.js
@@ -1,14 +1,14 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const PostSchema = new Schema({
   title: String,
   content: String,
-  tags: String,
+  tags: [String],
   cover_url: String,
 }, {
   toJSON: {
     virtuals: true,
-  }
+  },
 });
 
 const PostModel = mongoose.model('Post', PostSchema);
